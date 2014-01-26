3bef0c42aa4048e22c2c30a9df39472c2fe6dbb8
KodjoOhin <kohin@hubee.tv>
2014-01-26 01:39:00 +0100

    add directives file

---
+++ scripts/directives.js
@@ -0,0 +1,42 @@
+app.directive('expander', function () {
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        require: '^?accordion',
+        scope: { title: '=expanderTitle'},
+        template: '<div>' +
+            '<div class="title" ng-click="toggle()">{{title}}</div>' +
+            '<div class="body" ng-show="showMe" ng-transclude></div>' +
+            '</div>',
+        link: function (scope, element, attrs, accordionController) {
+            scope.showMe = false;
+            accordionController.addExpander(scope);
+            scope.toggle = function toggle() {
+                scope.showMe = !scope.showMe;
+                accordionController.gotOpened(scope);
+            }
+        }
+    }
+});
+app.directive('accordion', function () {
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        template: '<div ng-transclude></div>',
+        controller: function () {
+            var expanders = [];
+            this.gotOpened = function (selectedExpander) {
+                angular.forEach(expanders, function (expander) {
+                    if (selectedExpander != expander) {
+                        //expander.showMe = false;
+                    }
+                });
+            }
+            this.addExpander = function (expander) {
+                expanders.push(expander);
+            }
+        }
+    }
+});
