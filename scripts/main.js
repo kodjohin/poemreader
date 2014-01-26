bf30b26326638816221e5bbdcea4d0fc624025d6
KodjoOhin <kohin@hubee.tv>
2014-01-26 01:10:50 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,58 @@
 var fbUrl = 'https://kodjohin.firebaseio.com/';
-var app = angular.module('poemreader', ["ngSanitize", "firebase"]);
+var app = angular.module('poemreader', ["ngSanitize", "firebase", 'ngAnimate']);
 
-app.controller('MainCtrl', function($scope, $firebase){
+var search = document.querySelector('.search');
 
-   /* var fb = new Firebase(fbUrl + "poems");
-    fb.update(poems);*/
+app.controller('MainCtrl', function ($scope, $firebase) {
+
+    /* var fb = new Firebase(fbUrl + "poems");
+     fb.update(poems);*/
 
     var fb = $firebase(new Firebase(fbUrl));
 
-    $scope.navMenu      = fb.$child('navMenu');
-    $scope.poems        = fb.$child('poems');
+    $scope.navMenu = fb.$child('navMenu');
+    $scope.poems = fb.$child('poems');
+    $scope.showPoem = false;
 
-    $scope.navMenu.$on("loaded", function() {
-        $scope.activeMenu   = $scope.navMenu[0].name;
+    $scope.navMenu.$on("loaded", function () {
+        $scope.activeMenu = $scope.navMenu[0].name;
     });
 
-    $scope.poems.$on("loaded", function() {
-        $scope.poemTitle    = $scope.poems[0].title;
-        $scope.poemAuthor   = $scope.poems[0].author;
-        $scope.poemText     = $scope.poems[0].text;
+    $scope.poems.$on("loaded", function () {
+        $scope.poemTitle = $scope.poems[0].title;
+        $scope.poemAuthor = $scope.poems[0].author;
+        $scope.poemText = $scope.poems[0].text;
+
+        //display a random poem
+        var random = Math.floor(Math.random() * poems.length);
+        $scope.displayPoem($scope.poems[random]);
     });
 
-    $scope.displayPoem = function (poem){
+    $scope.displayPoem = function (poem) {
         $scope.poemTitle = poem.title;
         $scope.poemText = poem.text;
         $scope.poemAuthor = poem.author;
+        $scope.showPoem = true;
     }
 
-    $scope.displayItems = function (index){
-        $scope.currentAuthor = undefined;
-        $scope.currentMenu = $scope.navMenu[index].name;
-    }
-
-    $scope.displayPoems = function (poem){
+    $scope.displayPoems = function (poem) {
         $scope.currentAuthor = poem.author;
+        $scope.showPoem = false;
+    }
+
+    search.onfocus = function (){
+        $scope.currentAuthor = undefined;
+        $scope.showPoem = false;
     }
 });
-app.filter('unique', function() {
-    return function(collection, keyname) {
+app.filter('unique', function () {
+    return function (collection, keyname) {
         var output = [],
             keys = [];
 
-        angular.forEach(collection, function(item) {
+        angular.forEach(collection, function (item) {
             var key = item[keyname];
-            if(keys.indexOf(key) === -1) {
+            if (keys.indexOf(key) === -1) {
                 keys.push(key);
                 output.push(item);
             }
@@ -51,3 +60,4 @@ app.filter('unique', function() {
         return output;
     };
 });
+
